a07a2d68b9a45b90f02dc317dca4114d0a4f0987
steven2805 <stevenmac90@googlemail.com>
2019-08-31 12:17:46 +0100

    added webcam function

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 let net;
+const webcamElement = document.getElementById('webcam');
+
+async function setupwebcam(){
+  return new Promise((resolve,reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia || navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia || navigatorAny.msGetUSerMedia;
+    if(navigator.getUserMedia){
+      navigator.getUserMedia({Video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata', () => resolve(),false);
+
+        },
+        error = reject());
+    } else{
+      reject();
+    }
+  });
+}
+
+
 
 async function app(){
   console.log('Loading mobile.net !')
@@ -6,9 +27,15 @@ async function app(){
   net = await mobilenet.load();
   console.log("load success");
 
-  const imgEL = document.getElementById('img')
-  const result = await net.classify(imgEL)
-  console.log(result) 
+  await setupwebcam();
+  while (true){
+    const result = await net.classify(webcamElement);
+
+    document.getElementById('console').innerText = 'prediction: ${result[0].className\n probability: ${result[0].probability}';
+
+    await tf.nextFrame();
+  }
+
 }
 
 app()
